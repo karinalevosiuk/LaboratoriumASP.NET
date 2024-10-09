3941d84d54f21f38d2389431a025f59296bb1190
karina.levosiuk <karina.levosiuk@microsoft.wsei.edu.pl>
2024-10-09 12:13:44 +0200

    Imit

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -12,8 +12,60 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
+    //Zadanie domowe
+    //Napisz metode Age, ktora przyjmuje parametre z data urodzin i wyswietla wiek w latach, miesiacach i dniach
 
     public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Calculator(Operator? op, double? x, double? y)
+    {
+        //var op =Request.Query["op"];
+        //var x = double.Parse(Request.Query["x"]);
+        //var y = double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorError");
+        }
+        double? result = 0.0;
+        switch (op)
+        {
+            case Operator.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Mul:
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case Operator.Div:
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+                
+            
+        }
+        
+        ViewBag.Result = result;
+        ViewBag.x = x;
+        ViewBag.y = y;
+        
+        return View();
+    }
+    public IActionResult About()
     {
         return View();
     }
@@ -28,4 +80,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public enum Operator
+{
+    Add,Sub,Mul,Div
 }
